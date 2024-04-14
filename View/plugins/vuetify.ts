b71a0bf6b2d3008a5fc22bf55937be0cef3d82f9
yorimichi <yorimichi.dev@gmail.com>
2024-04-14 17:20:17 +0900

    feat: :art: add theme

--- View/plugins/vuetify.ts
+++ View/plugins/vuetify.ts
@@ -1,14 +1,14 @@
 // import this after install `@mdi/font` package
 import '@mdi/font/css/materialdesignicons.css';
 
+// Vuetify config
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 // custom theme
 import { THEME } from '@/theme/theme';
-
-// tabler icons
-
 
 // scss
 import '@/assets/scss/style.scss';
@@ -16,12 +16,49 @@ import '@/assets/scss/style.scss';
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     ssr: true,
+    components: {
+      ...components,
+    },
+    directives: directives,
     theme: {
-      defaultTheme: "THEME",
+      defaultTheme: 'THEME',
       themes: {
-        THEME
-      }
-    }
+        THEME,
+      },
+    },
+    defaults: {
+      VCard: {
+        rounded: 'md',
+        elevation: 10,
+      },
+
+      VTextField: {
+        variant: 'outlined',
+        density: 'comfortable',
+        color: 'primary',
+      },
+      VTextarea: {
+        variant: 'outlined',
+        density: 'comfortable',
+        color: 'primary',
+      },
+      VSelect: {
+        variant: 'outlined',
+        density: 'comfortable',
+        color: 'primary',
+      },
+      VListItem: {
+        minHeight: '45px',
+      },
+      VTooltip: {
+        location: 'top',
+      },
+      VBtn: {
+        style: 'text-transform: capitalize; letter-spacing:0',
+        rounded: 'md',
+      },
+    },
   });
+
   app.vueApp.use(vuetify);
 });
--- View/theme/theme.ts
+++ View/theme/theme.ts
@@ -1,37 +1,37 @@
 import type { ThemeTypes } from '@/theme/ThemeType';
 
 const THEME: ThemeTypes = {
-    name: 'THEME',
-    dark: false,
-    variables: {
-        'border-color': '#e5eaef'
-    },
-    colors: {
-        primary: '#5D87FF',
-        secondary: '#49BEFF',
-        info: '#539BFF',
-        success: '#13DEB9',
-        accent: '#FFAB91',
-        warning: '#FFAE1F',
-        error: '#FA896B',
-        lightprimary: '#ECF2FF',
-        lightsecondary: '#E8F7FF',
-        lightsuccess: '#E6FFFA',
-        lighterror: '#FDEDE8',
-        lightinfo:'#EBF3FE',
-        lightwarning: '#FEF5E5',
-        textPrimary: '#2A3547',
-        textSecondary: '#2A3547',
-        borderColor: '#e5eaef',
-        inputBorder: '#DFE5EF',
-        containerBg: '#ffffff',
-        background: '#ffffff',
-        hoverColor: '#f6f9fc',
-        surface: '#fff',
-        'on-surface-variant': '#fff',
-        grey100: '#F2F6FA',
-        grey200: '#EAEFF4'
-    }
+  name: 'THEME',
+  dark: false,
+  variables: {
+    'border-color': '#e5eaef',
+  },
+  colors: {
+    primary: '#5D87FF',
+    secondary: '#49BEFF',
+    info: '#539BFF',
+    success: '#13DEB9',
+    accent: '#FFAB91',
+    warning: '#FFAE1F',
+    error: '#FA896B',
+    lightprimary: '#ECF2FF',
+    lightsecondary: '#E8F7FF',
+    lightsuccess: '#E6FFFA',
+    lighterror: '#FDEDE8',
+    lightinfo: '#EBF3FE',
+    lightwarning: '#FEF5E5',
+    textPrimary: '#2A3547',
+    textSecondary: '#2A3547',
+    borderColor: '#e5eaef',
+    inputBorder: '#DFE5EF',
+    containerBg: '#ffffff',
+    background: '#ffffff',
+    hoverColor: '#f6f9fc',
+    surface: '#fff',
+    'on-surface-variant': '#fff',
+    grey100: '#F2F6FA',
+    grey200: '#EAEFF4',
+  },
 };
 
 export { THEME };
